15462d328b438455f96ed1cd3bcd6e0cb4b8abcb
DanielGolikov <golikov.danya@gmail.com>
2025-11-16 23:59:24 +1100

    Add test-specific configuration and exclude certain services from test profile

--- src/test/java/com/flashcardgroup/flashcard_backend/FlashcardBackendApplicationTests.java
+++ src/test/java/com/flashcardgroup/flashcard_backend/FlashcardBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.flashcardgroup.flashcard_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FlashcardBackendApplicationTests {
 
     @Test
